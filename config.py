e21d6477b2e6f4fa930339dd4a75101e32159999
Duong <tieuduong.25.1.98@gmail.com>
2026-05-22 05:56:16 +0700

    add send mail

--- config.py
+++ config.py
@@ -1,10 +1,5 @@
 import os
 
-MAIL_SERVER = "smtp.gmail.com"
-MAIL_PORT = 587
-MAIL_USE_TLS = True
-MAIL_USERNAME = "dttdthhixjve@gmail.com"
-MAIL_PASSWORD = "tfvztmctqsccvqtj"
 DATABASE_URL = "postgresql://postgres.etrgjnsffneohidiunhg:My_vnstock%40123@aws-1-ap-south-1.pooler.supabase.com:6543/postgres"
 
 RESEND_API_KEY = os.getenv("RESEND_API_KEY")
